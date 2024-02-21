dbd55123059995afdf62aac5e6e497f97f707e2f
Faraz Ahmad <102180103+FarazAhmad2@users.noreply.github.com>
2024-02-21 15:01:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
+	const date1_ms = Date.parse(date1);
+    const date2_ms = Date.parse(date2);
+    return Math.round((date2_ms - date1_ms) / (1000 * 60 * 60 * 24));
 };
 
 // Do not change the code below.
